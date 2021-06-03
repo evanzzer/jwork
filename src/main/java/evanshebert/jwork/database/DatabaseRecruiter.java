a26b37e1f84a2dec71de13005072b51c15d9ccd8
EvAnZz <evans.hebert@ui.ac.id>
2021-06-03 21:52:43 +0700

    Modul 9 - Tugas Tambahan - Recruiter Database in Postgresql

--- src/main/java/evanshebert/jwork/database/DatabaseRecruiter.java
+++ src/main/java/evanshebert/jwork/database/DatabaseRecruiter.java
@@ -2,83 +2,62 @@ package evanshebert.jwork.database;
 
 import java.util.ArrayList;
 
+import evanshebert.jwork.database.postgres.DatabaseRecruiterPostgres;
 import evanshebert.jwork.exceptions.RecruiterNotFoundException;
 import evanshebert.jwork.objects.Recruiter;
 
 /**
  * A list of database about Recruiter
- * 
+ *
  * @author Evans Hebert
- * @version 06 May 2021
+ * @version 03 June 2021
  */
-public class DatabaseRecruiter
-{
-    private static final ArrayList<Recruiter> RECRUITER_DATABASE = new ArrayList<>();
-    private static int lastId = 0;
-
+public class DatabaseRecruiter {
     /**
      * Retrieve a list of recruiter objects
+     *
      * @return A list of recruiter objects
      */
-    public static ArrayList<Recruiter> getRecruiterDatabase()
-    {
+    public static ArrayList<Recruiter> getRecruiterDatabase() {
         // Get listRecruiter
-        return RECRUITER_DATABASE;
+        return DatabaseRecruiterPostgres.getRecruiterDatabase();
     }
 
     /**
      * Retrieve the last ID of the database
+     *
      * @return Last ID in Integer
      */
-    public static int getLastId()
-    {
-        return lastId;
+    public static int getLastId() {
+        return DatabaseRecruiterPostgres.getLastRecruiterId();
     }
 
     /**
      * Retrieve a specified recruiter that can be found by ID
+     *
      * @return A recruiter object
      */
-    public static Recruiter getRecruiterById(int id) throws RecruiterNotFoundException
-    {
-        for (Recruiter recruiter : RECRUITER_DATABASE) {
-            if (recruiter.getId() == id) {
-                return recruiter;
-            }
-        }
-        throw new RecruiterNotFoundException(id);
+    public static Recruiter getRecruiterById(int id) throws RecruiterNotFoundException {
+        return DatabaseRecruiterPostgres.getRecruiterById(id);
     }
-    
+
     /**
      * Add a new recruiter
+     *
      * @param recruiter A recruiter Object
      * @return State to indicate a recruiter has been successfully added
      */
-    public static boolean addRecruiter(Recruiter recruiter)
-    {
-        RECRUITER_DATABASE.add(recruiter);
-        lastId = recruiter.getId();
-        return true;
+    public static boolean addRecruiter(Recruiter recruiter) {
+        return DatabaseRecruiterPostgres.insertRecruiter(recruiter);
     }
-    
+
     /**
      * Remove existing recruiter by ID
+     *
      * @param id A recruiter's ID
      * @return State to indicate a recruiter has been successfully removed
      */
-    public static boolean removeRecruiter(int id)
-    {
-        try {
-            for (Recruiter recruiter : RECRUITER_DATABASE) {
-                if (recruiter.getId() == id) {
-                    RECRUITER_DATABASE.remove(recruiter);
-                    return true;
-                }
-            }
-            throw new RecruiterNotFoundException(id);
-        } catch (RecruiterNotFoundException e) {
-            System.out.println(e.getMessage());
-            return false;
-        }
+    public static boolean removeRecruiter(int id) throws RecruiterNotFoundException {
+        return DatabaseRecruiterPostgres.deleteRecruiter(id);
     }
 }
---
+++ src/main/java/evanshebert/jwork/database/postgres/DatabaseRecruiterPostgres.java
@@ -0,0 +1,146 @@
+package evanshebert.jwork.database.postgres;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Calendar;
+
+import evanshebert.jwork.exceptions.RecruiterNotFoundException;
+import evanshebert.jwork.objects.Location;
+import evanshebert.jwork.objects.Recruiter;
+
+public class DatabaseRecruiterPostgres {
+    /**
+     * Retrieve a list of recruiter objects
+     *
+     * @return A list of recruiter objects
+     */
+    public static ArrayList<Recruiter> getRecruiterDatabase() {
+        ArrayList<Recruiter> list = new ArrayList<>();
+        try {
+            ResultSet rs = DatabaseConnectionPostgre.connection()
+                    .createStatement()
+                    .executeQuery("SELECT * FROM recruiter");
+
+            while (rs.next()) {
+                Recruiter recruiter = new Recruiter(
+                        rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getString(4),
+                        new Location(
+                                rs.getString(5),
+                                rs.getString(6),
+                                rs.getString(7)
+                        )
+                );
+                list.add(recruiter);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        return list;
+    }
+
+    /**
+     * Retrieve the last ID of the database
+     *
+     * @return Last ID in Integer
+     */
+    public static int getLastRecruiterId() {
+        int id = 0;
+        try {
+            ResultSet rs = DatabaseConnectionPostgre.connection()
+                    .createStatement()
+                    .executeQuery("SELECT MAX(id) FROM recruiter");
+
+            if (rs.next()) {
+                id = rs.getInt(1);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return id;
+    }
+
+    /**
+     * Retrieve a specified recruiter that can be found by ID
+     *
+     * @return A recruiter object
+     */
+    public static Recruiter getRecruiterById(int id) throws RecruiterNotFoundException {
+        try {
+            ResultSet rs = DatabaseConnectionPostgre.connection()
+                    .createStatement()
+                    .executeQuery("SELECT * FROM recruiter WHERE id = " + id);
+
+            if (rs.next()) {
+                Calendar calendar = Calendar.getInstance();
+                calendar.setTime(rs.getDate(5));
+                return new Recruiter(
+                        rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getString(4),
+                        new Location(
+                                rs.getString(5),
+                                rs.getString(6),
+                                rs.getString(7)
+                        )
+                );
+            } else throw new RecruiterNotFoundException(id);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * Add a new recruiter
+     *
+     * @param recruiter A recruiter Object
+     * @return State to indicate a recruiter has been successfully added
+     */
+    public static boolean insertRecruiter(Recruiter recruiter) {
+        try {
+            DatabaseConnectionPostgre.connection()
+                    .createStatement()
+                    .executeUpdate("INSERT INTO recruiter VALUES(" +
+                            recruiter.getId() + ", '" + recruiter.getName() + "', '" +
+                            recruiter.getEmail() + "', '" + recruiter.getPhoneNumber() + "', '" +
+                            recruiter.getLocation().getProvince() + "', '" +
+                            recruiter.getLocation().getCity() +"', '" +
+                            recruiter.getLocation().getDescription() + "')"
+                    );
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Remove existing recruiter by ID
+     *
+     * @param id A recruiter's ID
+     * @return State to indicate a recruiter has been successfully removed
+     */
+    public static boolean deleteRecruiter(int id) throws RecruiterNotFoundException {
+        Recruiter recruiter = getRecruiterById(id);
+        if (recruiter != null) {
+            try {
+                DatabaseConnectionPostgre.connection()
+                        .createStatement()
+                        .executeUpdate("DELETE FROM recruiter WHERE id = " + id);
+                return true;
+            } catch (SQLException e) {
+                e.printStackTrace();
+                return false;
+            }
+        } else throw new RecruiterNotFoundException(id);
+    }
+}
